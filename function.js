5293dd388dc67c664d76ce4e73c561f25e9b06b5
foresaa <133493061+foresaa@users.noreply.github.com>
2024-09-13 13:27:27 +0100

    Add files via upload

--- function.js
+++ function.js
@@ -1,12 +1,17 @@
+// Define the function that is called when the button is clicked
 function runPythonTask() {
-    fetch("https://mstpaddin.onrender.com/load-current-project-data/", {
-        method: "POST",
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.message);  // Display the response message in the console
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "https://mstpaddin.onrender.com/load-current-project-data/", true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      console.log("Task executed successfully!");
+    } else if (xhr.readyState === 4) {
+      console.error("Error executing task.");
+    }
+  };
+
+  // Sending request to FastAPI to trigger the task
+  xhr.send(JSON.stringify({}));
 }
